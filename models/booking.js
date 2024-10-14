2538e16ff5f8ce95741b5e49f592cd47a376761d
Mahesh Dahampath <daham2096@gmail.com>
2024-10-14 23:40:12 +0530

    created booking.js

---
+++ models/booking.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose'
+
+const bookingSchema = new mongoose.Schema({
+    bookingId : {
+        type : Number,
+        required : true,
+        unique : true
+    },
+
+    roomid : {
+        type : Number,
+        required : true
+    },
+
+    email : {
+        type : String,
+        required : true
+    },
+
+    status : {
+        type : "String",
+        required : true,
+        default : "pending"
+    },
+
+    reason : {
+        type : String,
+        default : ""
+    },
+
+    start : {
+        type : Date,
+        required : true
+    },
+
+    end : {
+        type : Date,
+        required : true
+    },
+
+    notes : {
+        type : String,
+        default : ""
+    }
+})
+
+const Booking = mongoose.model("booking",bookingSchema)
+export default Booking
