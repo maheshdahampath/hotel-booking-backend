f15cbbc9846920ad7a87beb6ecb4c462ca761849
Mahesh Dahampath <daham2096@gmail.com>
2024-10-14 10:38:51 +0530

    created room

---
+++ models/category.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema(
+    {
+        name : {
+            type: String,
+            required : true
+        },
+
+        description : {
+            type: String,
+            required : true
+        },
+
+        price : {
+            type: Int,
+            required : true
+        }
+    }
+)
+
+const Category = mongoose.model("category",categorySchema)
+export default Category
